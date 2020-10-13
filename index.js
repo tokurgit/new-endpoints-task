6e6dcdeb6b032d24c5fb8ff9a3f029f3aadda881
tokurgit <46890068+tokurgit@users.noreply.github.com>
2020-10-13 13:17:46 +0300

    Added an endpoint for calculating Excel sum

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const fs = require('fs');
-
+const { getXlsxStream } = require('xlstream');
+const multer = require('multer');
+const e = require('express');
 const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
 app.get('/', function (req, res)
@@ -10,6 +12,8 @@ app.get('/', function (req, res)
   res.send('Hello, World!');
 });
 
+
+//BTC PRICE GET ENDPOINT
 const getData = async (url) =>
 {
   try
@@ -21,39 +25,80 @@ const getData = async (url) =>
     console.log(error);
   }
 };
-//BTC PRICE ENDPOINT
-app.get('/btc', function (req, res)
+
+app.get('/btc', (req, res) =>
 {
   getData(url)
     .then((result) => res.send("Price of BTC is " + result.bpi.EUR.rate + " EUR"));
 });
+
 //CAPITAL CITY ENDPOINT
-app.get('/capital/:country', function (req, res)
+app.get('/capital/:country', (req, res) =>
 {
-
-  let capitalCities = fs.readFileSync('node_modules/country-json/src/country-by-capital-city.json', 'utf8',
-    (err, data) => 
-    {
-      if (err) throw err;
-      return data;
-    });
-
   let searchValue = req.params.country.toLowerCase();
   let searchField = "country";
   let searchCapitalCityField = "city";
-  let parsedCollection = JSON.parse(capitalCities);
   let capitalCity;
+  let country;
+  let message;
 
-  for (let i = 0; i < parsedCollection.length; i++) 
-  {
-    let searchCountry = parsedCollection[i][searchField].toLowerCase();
-    if (searchCountry == searchValue)
+  fs.readFile('node_modules/country-json/src/country-by-capital-city.json', 'utf8',
+    (err, data) => 
     {
-      capitalCity = parsedCollection[i][searchCapitalCityField];
-    }
-  }
+      if (err)
+      {
+        throw err;
+      }
+      else
+      {
+        let parsedData = JSON.parse(data);
+        for (let i = 0; i < parsedData.length; i++) 
+        {
+          let searchCountry = parsedData[i][searchField].toLowerCase();
+          if (searchCountry == searchValue)
+          {
+            country = parsedData[i][searchField];
+            capitalCity = parsedData[i][searchCapitalCityField];
+            message = `Capital city of ${country} is ${capitalCity}`;
+            break;
+          }
+          else
+          {
+            message = "We couldn't find such a country";
+          }
+        }
+        res.send(message);
+      }
+    });
+});
 
-  res.send("Capital city of Latvia is " + capitalCity);
+//EXCEL SUM POST ENDPOINT
+var storage = multer.diskStorage({
+  destination: function (req, file, cb)
+  {
+    cb(null, __dirname + '/uploads/');
+  },
+  filename: function (req, file, cb)
+  {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+var upload = multer({
+  storage: storage
+});
+
+app.post('/excel-sum', upload.single('file'), (req, res) =>
+{
+  (async () =>
+  {
+    const stream = await getXlsxStream({
+      filePath: req.file.path,
+      sheet: 0,
+    });
+    let dataArr = [];
+    stream.on('data', x => dataArr.push(x.raw.obj.A)),
+      stream.on('end', () => res.send(`SUM is ${dataArr.reduce((a, b) => a + b)}`));
+  })();
 });
 
 app.listen(3000);
